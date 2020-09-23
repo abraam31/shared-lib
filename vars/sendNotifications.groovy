17079c75d8975abc4222c5736102729432cc9d9a
MAGED Abraam <Abraam.maged@cegedim.com>
2020-09-23 12:57:36 +0200

    complex lib

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -1,12 +1,23 @@
-def call(Map config = [:]) {
-	slackSend (
-		    color: "${config.slackSendColor}",
-    		    message: "${config.message}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})"
-  		)
-	emailext (
-		    subject: "${config.message}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-    		    body: """<p>${config.message}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-<p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""",
-    		    recipientProviders: [[$class: 'DevelopersRecipientProvider']]
-		 )
+def generateTemplate(input, vars) {
+	def engine = new.groovy.text.GStringTemplate()
+	def template = engine.createTemplate(input).make(binding) 
+	return template.toString()
 }
+
+
+def call(Map config = [:]) {
+	def rawBody = libraryResource 'emailtemplates/build-results.html'
+	def binding = [
+		applicationName: env.JOB_NAME
+		buildNumber: env.BUILD_NUMBER
+		buildURL: env.BUILD_URL
+		message: config.message
+		]
+	def emailbody = generateTemplate(rawBody,binding)
+
+	emailext (
+		subject: "${config.message}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+		body: emailBody,
+		recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+		)
+	}
