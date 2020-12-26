86e47a5b85fa00e363e35bf65e03ee6bfb80d924
uda-abraam <abraam.maged@cegedim.com>
2020-12-26 20:03:48 +0200

    adding in vars

--- vars/yaml.groovy
+++ vars/yaml.groovy
@@ -5,27 +5,26 @@ def call(String configFile) {
 		// notifications vars 
 		env.notificationRecipient = datas.notifications.email.recipients.toString()
 		env.notificationOnStart = datas.notifications.email.on_start.toString()
-		notificationOnFailure = datas.notifications.email.on_failure.toString()
-		notificationOnSuccess = datas.notifications.email.on_success.toString()
+		env.notificationOnFailure = datas.notifications.email.on_failure.toString()
+		env.notificationOnSuccess = datas.notifications.email.on_success.toString()
 
 		// test vars 
-		testFolder = datas.test[0].testFolder.toString()
+		env.testFolder = datas.test[0].testFolder.toString()
 
-		performanceTestCommand = datas.test[0].testCommand.toString()
-		regressionTestCommand = datas.test[1].testCommand.toString()
-		integrationTestCommand = datas.test[2].testCommand.toString()
+		env.performanceTestCommand = datas.test[0].testCommand.toString()
+		env.regressionTestCommand = datas.test[1].testCommand.toString()
+		env.integrationTestCommand = datas.test[2].testCommand.toString()
 
 		// build vars 
-		buildCommand = datas.build.buildCommand.toString()
-		buildProjectFolder = datas.build.projectFolder.toString()
+		env.buildCommand = datas.build.buildCommand.toString()
+		env.buildProjectFolder = datas.build.projectFolder.toString()
 		
 		// Database vars 
-		databaseCommand = datas.database.databaseCommand.toString()
-		databaseProjectFolder = datas.database.databaseFolder.toString()
+		env.databaseCommand = datas.database.databaseCommand.toString()
+		env.databaseProjectFolder = datas.database.databaseFolder.toString()
 
 		// deploy vars 
-		deployProjectFolder = datas.build.projectFolder.toString()
-		deployCommand = datas.deploy.deployCommand.toString()
-		echo "Send on start: ${notificationOnStart}"
+		env.deployProjectFolder = datas.build.projectFolder.toString()
+		env.deployCommand = datas.deploy.deployCommand.toString()
 	}
 }
