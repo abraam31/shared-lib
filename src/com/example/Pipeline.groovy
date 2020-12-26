e4977549acb64c97d7e3f78f32bbf5df6162b3cc
abraam31 <52758969+abraam31@users.noreply.github.com>
2020-12-26 17:09:18 +0200

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -1,3 +1,8 @@
+@Grab('org.yaml:snakeyaml:1.17')
+import org.yaml.snakeyaml.Yaml
+import org.yaml.snakeyaml.DumperOptions
+import static org.yaml.snakeyaml.DumperOptions.FlowStyle.BLOCK
+
 package com.example
 
 class Pipeline {
