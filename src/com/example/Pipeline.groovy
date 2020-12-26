b646e3d50f86a1d898ffbe18a7b17016a17ef847
uda-abraam <abraam.maged@cegedim.com>
2020-12-26 18:32:49 +0200

    adding in vars

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -1,6 +1,5 @@
 package com.example
 
-@Grab('org.yaml:snakeyaml:1.17')
 import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.DumperOptions
 import static org.yaml.snakeyaml.DumperOptions.FlowStyle.BLOCK
