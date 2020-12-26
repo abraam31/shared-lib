829af22a79aca14d7b60dc2abd117de2d03af488
uda-abraam <abraam.maged@cegedim.com>
2020-12-26 21:12:37 +0200

    Trying second solution - groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -1,5 +1,8 @@
 package com.example
 
+@Grab('org.yaml:snakeyaml:1.17')
+import org.yaml.snakeyaml.Yaml
+	
 class Pipeline {
     def script
     def configurationFile
@@ -13,32 +16,12 @@ class Pipeline {
 //    ===================== Your Code Starts Here =====================
 //    Note : use "script" to access objects from jenkins pipeline run (WorkflowScript passed from Jenkinsfile)
 //           for example: script.node(), script.stage() etc
-		datas = readYaml (file: 'config.yml') 
+		Yaml parser = new Yaml()
+		List example = parser.load(("config.yml" as File).text)
+		example.each{println it.subject}
 		
-		// notifications vars 
-		notificationRecipient = datas.notifications.email.recipients.toString()
-		notificationOnStart = datas.notifications.email.on_start.toString()
-		notificationOnFailure = datas.notifications.email.on_failure.toString()
-		notificationOnSuccess = datas.notifications.email.on_success.toString()
 
-		// test vars 
-		testFolder = datas.test[0].testFolder.toString()
 
-		performanceTestCommand = datas.test[0].testCommand.toString()
-		regressionTestCommand = datas.test[1].testCommand.toString()
-		integrationTestCommand = datas.test[2].testCommand.toString()
-
-		// build vars 
-		buildCommand = datas.build.buildCommand.toString()
-		buildProjectFolder = datas.build.projectFolder.toString()
-		
-		// Database vars 
-		databaseCommand = datas.database.databaseCommand.toString()
-		databaseProjectFolder = datas.database.databaseFolder.toString()
-
-		// deploy vars 
-		deployProjectFolder = datas.build.projectFolder.toString()
-		deployCommand = datas.deploy.deployCommand.toString()
 //    ===================== Parse configuration file ==================
 
 //    ===================== Run pipeline stages =======================
